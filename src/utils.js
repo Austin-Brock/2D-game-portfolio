33babb597ac6638188e840d7d97a6d5d9614ceb6
Austin-Brock <austin24brock@gmail.com>
2024-04-16 15:01:14 -0400

    updated index.html and utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,26 @@
 export function displayDialogue (text, onDisplayEnd) {
     const dialogueUI = document.getElementById("textbox-container");
+    const dialogue = document.getElementById("dialogue");
+
+    dologueUI.style.display = "block";
+
+    let index = 0;
+    let currentText = "";
+    const intervalRef = setInterval(() => {
+        if (index < text.length) {
+            currentText += text[index];
+            dialogue.innerHTML = currentTextl
+            index++;
+            return;
+        }
+        clearInterval(intervalRef);
+    }, 5);
+    function onCloseBtnClick() {
+        onDisplayEnd();
+        dialogueUI.style.display = "none";
+        dialogue.innerHTML = "";
+        clearInterval(intervalRef);
+        onCloseBtn.removeEventListener("click", onCloseBtnClick);
+    }
+    onCloseBtnClick.addEventListener("click", onCloseBtnClick);
 }
