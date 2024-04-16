dc7c1bda16a6d365f1c4987f10befd8b603f6a20
Austin-Brock <austin24brock@gmail.com>
2024-04-16 15:37:33 -0400

    added constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,15 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work with many languages and different software to create a lot of projects`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. I haven't gotten to use it a lot but I like watching Criminal Minds, Silicon Valley, and Game of thrones :`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="" target="_blank">Check it out?</a>
+  Contact me at Austin24brock@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves.`,
+  exit: `If you want to exit Austins portfolio, just close the tab.`,
+};
